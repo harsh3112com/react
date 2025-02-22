b4987ad24695f11c2c3baf96ce9860d86837ef83
harsh3112com <harshbedi3112@gmail.com>
2025-02-22 20:19:38 +0530

    react

---
+++ secondclass/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+ return (
+  <>
+   <h1>Welcome to React class</h1>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore non dignissimos inventore sint 
+    eum nostrum rerum quos consequatur, minus eveniet, ipsa nobis maiores! Ducimus et sapiente atque voluptate itaque asperiores.</p>
+    <img src="agra.jpeg" alt="" />
+  </>
+ )
+}
+
+export default App
