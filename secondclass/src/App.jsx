b0d003777a68c35aa3d50f3539bf2f9cec0efd68
harsh3112com <harshbedi3112@gmail.com>
2025-02-24 19:07:21 +0530

    css in react

--- secondclass/src/App.jsx
+++ secondclass/src/App.jsx
@@ -1,10 +1,34 @@
+import Myimg from './agra.jpeg'
+import './App.css'
 function App() {
+  let name="aryan"
+  let age=24
+
+  let st={
+    backgroundColor:"black",
+    padding:"20px",
+    display:"flex"
+  }
  return (
   <>
    <h1>Welcome to React class</h1>
    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore non dignissimos inventore sint 
     eum nostrum rerum quos consequatur, minus eveniet, ipsa nobis maiores! Ducimus et sapiente atque voluptate itaque asperiores.</p>
-    <img src="agra.jpeg" alt="" />
+    <img src={Myimg} alt="" />
+    <h1 style={{backgroundColor:"red",color:"white"}}>My name is {name}</h1>
+    <h1 style={st}>my age is {age}</h1>
+    <h1 className='ex'>hello</h1>
+
+    <nav className='nav1'>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contac</li>
+      </ul>
+     
+
+    </nav>
+
   </>
  )
 }
