d98692223bb01f0853a6b12ed0c4b4956885b060
harsh3112com <harshbedi3112@gmail.com>
2025-02-25 19:18:03 +0530

    multipage

--- secondclass/src/App.jsx
+++ secondclass/src/App.jsx
@@ -1,36 +1,50 @@
-import Myimg from './agra.jpeg'
-import './App.css'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Gallery from './Gallery.jsx'
+import Services from './Services.jsx'
 function App() {
-  let name="aryan"
-  let age=24
+  return(
+    <>
+    <h1>Welcome to React Class</h1>
+    <About/>
+    <Contact/>
+    <Gallery/>
+    <Services/>
 
-  let st={
-    backgroundColor:"black",
-    padding:"20px",
-    display:"flex"
-  }
- return (
-  <>
-   <h1>Welcome to React class</h1>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore non dignissimos inventore sint 
-    eum nostrum rerum quos consequatur, minus eveniet, ipsa nobis maiores! Ducimus et sapiente atque voluptate itaque asperiores.</p>
-    <img src={Myimg} alt="" />
-    <h1 style={{backgroundColor:"red",color:"white"}}>My name is {name}</h1>
-    <h1 style={st}>my age is {age}</h1>
-    <h1 className='ex'>hello</h1>
+    </>
+  )
 
-    <nav className='nav1'>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contac</li>
-      </ul>
+
+//   let name="aryan"
+//   let age=24
+
+//   let st={
+//     backgroundColor:"black",
+//     padding:"20px",
+//     display:"flex"
+//   }
+//  return (
+//   <>
+//    <h1>Welcome to React class</h1>
+//    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore non dignissimos inventore sint 
+//     eum nostrum rerum quos consequatur, minus eveniet, ipsa nobis maiores! Ducimus et sapiente atque voluptate itaque asperiores.</p>
+//     <img src={Myimg} alt="" />
+//     <h1 style={{backgroundColor:"red",color:"white"}}>My name is {name}</h1>
+//     <h1 style={st}>my age is {age}</h1>
+//     <h1 className='ex'>hello</h1>
+
+//     <nav className='nav1'>
+//       <ul>
+//         <li>Home</li>
+//         <li>About</li>
+//         <li>Contac</li>
+//       </ul>
      
 
-    </nav>
+//     </nav>
 
-  </>
- )
+//   </>
+//  )
 }
 
 export default App
